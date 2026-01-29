a2ae0dfd4f873f05057e91f58c76fb29cfd2d732
connorcaldwell20255-lgtm <connorcaldwell20255@gmail.com>
2026-01-28 20:34:36 -0500

    chore: remove deprecated .eslintignore and keep flat config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  settings: {
+    react: { version: 'detect' }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  overrides: [
+    { files: ['*.ts', '*.tsx'], parser: '@typescript-eslint/parser' }
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+};
